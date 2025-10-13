724cce2f75c380b4159173fcbbfc5acb779f367c
someone225 <marksheng2020@gmail.com>
2025-10-13 12:01:42 -0400

    update

--- Lib/src/Team19CustomUtils/images.py
+++ Lib/src/Team19CustomUtils/images.py
@@ -1,3 +1,4 @@
+import math as m
 def a_reduce_dim(input):
     output = [0] *( len(input) * len(input[0]) )
     k = 0
@@ -6,3 +7,22 @@ def a_reduce_dim(input):
             output[k] = input[i][j]
             k += 1
     return output
+
+def normalize(ch_input):
+    """
+    normalizes an input array according to the rgb linearlization algorithm
+    Args:
+        ch_input: a 2d array containing pixel values of one rgb channel
+
+    Returns: 
+        void
+
+    Dependencies: 
+        requires 'pow' module from library 'math' to linearlize bright pixels
+    """
+    for i in range(0, len(ch_input)):
+        for j in range(0, len(ch_input[0])):
+            if(ch_input[i][j] <= 0.0405):
+                ch_input[i][j] = ch_input[i][j] / 12.92
+            else:
+                ch_input[i][j] = m.pow( ((ch_input[i][j] + 0.055)/1.055), 2.4 )  
