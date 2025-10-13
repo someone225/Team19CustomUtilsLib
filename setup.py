ff427e0f2d1cad39a21443d2742c963f4e857c0c
someone225 <marksheng2020@gmail.com>
2025-10-13 12:21:15 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Team19CustomUtilsLib',
+   version='1.1',
+   description='library containing some useful functions for team projects of engr133',
+   author='Mark Sheng',
+   author_email='marksheng2020@gmai.com',
+   packages=['Team19CustomUtilsLib'],  #same as name
+   install_requires=['math'], #external packages as dependencies
+)
